7c64dc8b66a0c58895640b9488114eea2f0d7782
syl <2234746866@qq.com>
2025-09-19 15:45:52 +0800

    命名空间隔离、控制器调整为RPP

--- akermann/launch/display_robot.launch.py
+++ akermann/launch/display_robot.launch.py
@@ -4,55 +4,16 @@ import os
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.parameter_descriptions
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import GroupAction, DeclareLaunchArgument
+from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
     print("启动文件已加载，准备启动机器人相关节点！")
+    robots = ['robot1']  # 定义多个机器人名称
+    robots_init_xyz = {'robot1' : ['-10.0', '-5.5', '0.1'], 'robot2' : ['-12.0', '-5.5', '0.1']}  # 定义每个机器人的初始位置
     urdf_pkg_path = get_package_share_directory('ackermann')
     default_urdf_path = os.path.join(urdf_pkg_path, 'urdf', 'robot.urdf.xacro')
-    default_rviz_config_path = os.path.join(urdf_pkg_path, 'config', 'dispaly_model.rviz')
     default_world_config_path = os.path.join(urdf_pkg_path, 'world', 'simple_world.world')
-    
-    action_declare_robot_description = launch.actions.DeclareLaunchArgument(
-        name='model',
-        default_value= str(default_urdf_path),
-        description='Absolute path to robot urdf file',
-    )
-
-    #通过文件读取URDF内容，并传递给robot_state_publisher节点
-    command = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
-    value = launch_ros.parameter_descriptions.ParameterValue(command, value_type=str)
-
-    action_robot_state_publisher = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': value, 'use_sim_time': True}],
-    )
-
-    # action_joint_state_publisher = launch_ros.actions.Node(
-    #     package='joint_state_publisher',
-    #     executable='joint_state_publisher', # 这是传参给joint_state_publisher的可执行文件
-    #     parameters=[{
-    #         'use_sim_time': True
-    #     }]
-    # )
-
-    action_rviz2_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', default_rviz_config_path], # 相当于命令行参数 -d
-    )
-
-    action_spawn_entity = launch_ros.actions.Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'ackermann',
-            '-topic', 'robot_description',
-            '-x', '-10.0',  # 设置初始位置x坐标
-            '-y', '-5.5',  # 设置初始位置y坐标
-            '-z', '0.1'   # 设置初始位置z坐标
-        ],
-    )
 
     action_launch_gazebo = launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource([get_package_share_directory(
@@ -60,11 +21,33 @@ def generate_launch_description():
         launch_arguments=[('world', default_world_config_path), ('verbose', 'true')],  # 设置world文件路径和verbose模式
     )
 
+    groups = []
+
+    for robot in robots:
+        command = launch.substitutions.Command(['xacro ', default_urdf_path, ' robot_name:=', robot])
+        value = launch_ros.parameter_descriptions.ParameterValue(command, value_type=str)
+
+        groups.append( GroupAction([
+            PushRosNamespace(robot),
+            launch_ros.actions.Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                arguments=[
+                    '-entity', robot,
+                    '-topic', 'robot_description',
+                    '-x', robots_init_xyz[robot][0],  # 设置初始位置x坐标
+                    '-y', robots_init_xyz[robot][1],  # 设置初始位置y坐标
+                    '-z', robots_init_xyz[robot][2]   # 设置初始位置z坐标
+                ],
+            ),
+            launch_ros.actions.Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                parameters=[{'robot_description': value, 'use_sim_time': True}],
+            ),
+        ])  
+        )
+
     return launch.LaunchDescription([
-        action_declare_robot_description,
-        # action_joint_state_publisher,
         action_launch_gazebo,
-        action_spawn_entity,
-        action_robot_state_publisher,
-        # action_rviz2_node,
-    ])
+    ] + groups)
--- akermann/launch/navigations.launch.py
+++ akermann/launch/navigations.launch.py
@@ -5,33 +5,32 @@ from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import ExecuteProcess, RegisterEventHandler, EmitEvent
 from launch.event_handlers import OnProcessExit
+from launch_ros.actions import PushRosNamespace
 
 
 def generate_launch_description():
     # Get the path to the package
-    gunnerycar_pkg_path = get_package_share_directory('ackermann')
-    
-    bringup_pkg_path = get_package_share_directory('nav2_bringup')
+    ackermann_pkg_path = get_package_share_directory('ackermann')
 
-    rviz_config_path = os.path.join(bringup_pkg_path, 'rviz', 'nav2_default_view.rviz')
+    robots = ['robot1', 'robot2']  # Define multiple robot names
+
+    rviz_config_path = os.path.join(ackermann_pkg_path, 'config', 'nav2_namespaced_view.rviz')
 
     use_sim_time = launch.substitutions.LaunchConfiguration(
         'use_sim_time', default='true')
     
     map_yaml_path = launch.substitutions.LaunchConfiguration(
-        'map', default = os.path.join(gunnerycar_pkg_path, 'map', 'simple_room.yaml'))
+        'map', default = os.path.join(ackermann_pkg_path, 'map', 'simple_room.yaml'))
     
     init_pose_path = launch.substitutions.LaunchConfiguration(
-        'init_pose', default = os.path.join(gunnerycar_pkg_path, 'config', 'init_pose_node.yaml'))
-    
-    follow_path_path = launch.substitutions.LaunchConfiguration(
-        'waypoints', default = os.path.join(gunnerycar_pkg_path, 'config', 'follow_path_node.yaml'))
+        'init_pose', default = os.path.join(ackermann_pkg_path, 'config', 'init_pose_node.yaml'))
     
     nav2_param_path = launch.substitutions.LaunchConfiguration(
-        'nav2_param_file', default = os.path.join(gunnerycar_pkg_path, 'config', 'nav2_params.yaml'))
+        'nav2_param_file', default = os.path.join(ackermann_pkg_path, 'config', 'nav2_params.yaml'))
     
     action_init_pose = launch_ros.actions.Node(
-            package='gunnerycar',
+            namespace='robot1',  # 这里加命名空间
+            package='ackermann',
             executable='init_pose_node',
             name='init_pose_node',
             output='screen',
@@ -39,19 +38,14 @@ def generate_launch_description():
         )
     
     action_send_goal = launch_ros.actions.Node(
-            package='gunnerycar',
-            executable='follow_path_node',
-            name='follow_path_node',
+            namespace='robot1',  # 这里加命名空间
+            package='ackermann',
+            executable='send_goal_node',
+            name='send_goal_node',
             output='screen',
-            parameters = [follow_path_path],
+            parameters = [init_pose_path],
         )
     
-    action_speaker = launch_ros.actions.Node(
-            package='gunnerycar',
-            executable='speaker_service',
-            name='speaker_service',
-            output='screen',
-        )
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
@@ -62,12 +56,14 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument("params_file", default_value= nav2_param_path, description="Full path to the ROS2 parameters file to load"),
         launch.actions.IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [bringup_pkg_path, '/launch', '/bringup_launch.py']),
+                [ackermann_pkg_path, '/launch', '/bringup_launch.py']),
 
             launch_arguments={
                 'map': map_yaml_path,
                 'use_sim_time': use_sim_time,
-                'params_file': nav2_param_path
+                'params_file': nav2_param_path,
+                'namespace': 'robot1',
+                'use_namespace': 'true',
             }.items(),
         ),
         launch_ros.actions.Node(
@@ -80,12 +76,5 @@ def generate_launch_description():
         ),
 
         action_init_pose,
-        # action_send_goal,
-        # action_speaker,
-        # launch.actions.RegisterEventHandler(
-        #     event_handler=launch.event_handlers.OnExecutionComplete(
-        #         target_action=action_init_pose,
-        #         on_completion=[action_send_goal]
-        #     )
-        # ),
+        action_send_goal,
     ])
